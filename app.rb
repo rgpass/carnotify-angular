a8fa6ae0d4c711c58ac2276a59bac2b85d864f00
rgpass <rgpass@gmail.com>
2015-03-01 18:16:29 -0500

    Cleanup

--- app.rb
+++ app.rb
@@ -1,20 +1,41 @@
+# # require 'sinatra'
+# # require 'slim'
+
+# # get '/' do
+# #   slim :main
+# # end
+
+# Bundler.require
+
 # require 'sinatra'
 # require 'slim'
+# require 'sinatra/asset_pipeline'
 
-# get '/' do
-#   slim :main
+# class App < Sinatra::Base
+#   register Sinatra::AssetPipeline
+
+#   get '/' do
+#     slim :main
+#   end
 # end
 
+require 'rubygems'
+require 'bundler'
 Bundler.require
 
-require 'sinatra'
-require 'slim'
 require 'sinatra/asset_pipeline'
 
 class App < Sinatra::Base
+  # Logical paths to your assets
+  set :assets_prefix, %w(assets vendor/assets)
+
   register Sinatra::AssetPipeline
 
   get '/' do
     slim :main
+  end
+
+  get '/api/v1/*' do
+    HTTParty.get("https://carnotify-api.herokuapp.com/api/v1/#{params[:splat].first}").to_json
   end
 end
--- config.ru
+++ config.ru
@@ -1,22 +1,2 @@
-require 'rubygems'
-require 'bundler'
-Bundler.require
-
-require 'sinatra/asset_pipeline'
-
-class App < Sinatra::Base
-  # Logical paths to your assets
-  set :assets_prefix, %w(assets vendor/assets bower_components angular)
-
-  register Sinatra::AssetPipeline
-
-  get '/' do
-    slim :main
-  end
-
-  get '/api/v1/*' do
-    HTTParty.get("https://carnotify-api.herokuapp.com/api/v1/#{params[:splat].first}").to_json
-  end
-end
-
+require './app'
 run App
