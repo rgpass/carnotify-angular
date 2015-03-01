858977d115c6dd6f140f88ef53ce593bbe5e6156
rgpass <rgpass@gmail.com>
2015-03-01 18:18:11 -0500

    Final cleanup

--- app.rb
+++ app.rb
@@ -1,24 +1,3 @@
-# # require 'sinatra'
-# # require 'slim'
-
-# # get '/' do
-# #   slim :main
-# # end
-
-# Bundler.require
-
-# require 'sinatra'
-# require 'slim'
-# require 'sinatra/asset_pipeline'
-
-# class App < Sinatra::Base
-#   register Sinatra::AssetPipeline
-
-#   get '/' do
-#     slim :main
-#   end
-# end
-
 require 'rubygems'
 require 'bundler'
 Bundler.require
