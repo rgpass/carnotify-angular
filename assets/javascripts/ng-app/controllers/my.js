ba14d4edbb90957092dfc66f55a7e5ee7577cf0a
rgpass <rgpass@gmail.com>
2015-03-03 09:13:56 -0500

    Add ng services

--- assets/javascripts/ng-app/controllers/my.js
+++ assets/javascripts/ng-app/controllers/my.js
@@ -1,4 +1,4 @@
 angular.module('myApp')
-.controller('myCtrl', ['$scope', function ($scope) {
-  $scope.query = 'blah';
+.controller('myCtrl', ['$scope', 'myService', function ($scope, myService) {
+  $scope.query = myService.query;
 }]);
